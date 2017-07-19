754569d523c87c65bbb0b8211477b4254c64c7e7
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-07-18 22:22:36 -0700

    Calculating the properties os a circle

---
+++ theGeometrizer.js
@@ -0,0 +1,23 @@
+(function() {
+	var radius = 3;
+	var pi = 3.1416;
+
+	var circumference = (2 * pi * radius);
+	var area = (pi * Math.pow(radius, 2)); 
+
+	console.log('The circumference is ', circumference);
+	console.log('The area is ', area);
+
+	// The circumference is  18.8496
+	// The area is  28.2744
+
+})();
+
+/* The Geometrizer
+
+Calculate properties of a circle.
+
+Store a radius into a variable.
+Calculate the circumference based on the radius, and output "The circumference is NN".
+Calculate the area based on the radius, and output "The area is NN". */
+
